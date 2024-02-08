5ba57b235e6159aa1838b4a08557bd0f784d8c2c
BichalaDeepthi <150236108+BichalaDeepthi@users.noreply.github.com>
2024-02-08 17:38:53 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -18,44 +18,21 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include "INT_Flash.h"
-int _write(int file,char *ptr,int len)
-{
-  int i=0;
-for(i=0;i<len;i++)
-  ITM_SendChar((*ptr++));
-return len;
-}
+#include "stdio.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "INT_Flash.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-typedef struct gpsdata
-{
-	double time;
-	float latitude;
-	float longitude;
-	int Date;
-	char N_OR_S;
-	char E_OR_W;
-	int fix;
-	char Data[750];
-	char buffer[100];
-	char rmc_buffer[100];
-}gpsdata;
 
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define Min_To_Degree  0.01666666667
-#define Sec_To_Degree  0.000277777778
+#define FLASH_START_ADDRESS 0x08080000
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -64,194 +41,20 @@ typedef struct gpsdata
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-UART_HandleTypeDef huart4;
-UART_HandleTypeDef huart5;
-UART_HandleTypeDef huart1;
-UART_HandleTypeDef huart2;
-UART_HandleTypeDef huart3;
 
 /* USER CODE BEGIN PV */
-char gps_info[100],gps_comp_info[100];
-char str[100];
-gpsdata gps;
-gpsdata gps_rcv;
-int dd,mm,yy;
-int flag=0;
-int GMT=530;
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_UART4_Init(void);
-static void MX_UART5_Init(void);
-static void MX_USART1_UART_Init(void);
-static void MX_USART2_UART_Init(void);
-static void MX_USART3_UART_Init(void);
 /* USER CODE BEGIN PFP */
-void gpsData(gpsdata* data);
-void gpslocation_extraction(gpsdata* data);
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
-{
-    if (huart == &huart1)
-    {
-        // Process the received data
-    	printf("data is %s\n",gps.Data);
-        gpsData(&gps);
-    }
-}
 
-static int gps_init(void)
-{
-	if(HAL_UART_Receive(&huart5,(uint8_t *)gps.Data,sizeof(gps.Data),1000))
-	{
-	      printf("%s\n",gps.Data);
-	       gpsData(&gps);
-	       return 1;
-	}
-	else
-	{
-
-		printf("Uart data not received\n");
-		return 0;
-	}
-}
-void gpsData(gpsdata *data)
-{
-	char *gga_ptr,*rmc_ptr;
-	int n;
-	printf("in func\n");
-	//memset(data->Data,0,sizeof(data->Data));
-	//HAL_UART_Receive(&huart3, data->Data, sizeof(data->Data),5000);
-	//printf("string %s\n",data->Data);
-	gga_ptr=strstr(data->Data,"GNGGA");
-	rmc_ptr=strstr(data->Data,"GNRMC");
-	//printf("string found\n");
-	if(gga_ptr!=NULL)
-	{
-	if(*gga_ptr=='G')
-	{
-	   n=0;
-	   while(*(gga_ptr+n)!='*')
-	   {
-		   data->buffer[n]=*(gga_ptr+n);
-		   n++;
-	   }
-	    printf("GGA string is %s", data->buffer);
-	//	sprintf(data->buffer,"%s",data->buffer);
-		sscanf(data->buffer,"GNGGA,%lf,%f,%c,%f,%c,%d,",&data->time,&data->latitude,&data->N_OR_S,&data->longitude,&data->E_OR_W,&data->fix);
-	}
-	else
-		{
-			printf("GGA string not found\n");
-		}
-
-	}
-	else
-	{
-		printf("GGA string not found\n");
-	}
-
-	if(rmc_ptr!=NULL)
-		{
-		if(*rmc_ptr=='G')
-				{
-				   n=0;
-				   while(*(rmc_ptr+n)!='*')
-				   {
-					   data->rmc_buffer[n]=*(rmc_ptr+n);
-					   n++;
-				   }
-				   printf("RMC string is %s", data->rmc_buffer);
-				   					//sprintf(data->buffer,"%s",data->location);
-				   					sscanf(data->rmc_buffer,"GNRMC,%*lf,%*c,%*f,%*c,%*f,%*c,%*f,%*f,%d,",&data->Date);
-				   					printf("date is %d\n",data->Date);
-				}
-
-
-	else
-	{
-		printf("RMC string not found\n");
-	}
-		}
-	else
-		{
-			printf("RMC string not found\n");
-		}
-
-
-					//memset(data->Data,0,sizeof(data->Data));
-
-
-		if(data->fix==1)
-		{
-			printf("valid fix\n");
-			gpslocation_extraction(data);
-		}
-		else
-		{
-			sprintf(gps_info, "%s","invalid signal no fix, Unable to obtain accurate location.\n");
-		}
-
-}
-
-void gpslocation_extraction(gpsdata* data)
-{
-	double Deg_Val=0.0,Min_Val=0.0,Sec_Val=0.0,lon=0.0,lat=0.0;
-	int hr,min,sec;
-	lon=data->longitude;
-	lat=data->latitude;
-	if((data->E_OR_W=='E' || data->N_OR_S=='S')||(data->E_OR_W=='W' || data->N_OR_S=='N'))
-	{
-		Deg_Val=(int)(lon/100);
-		Min_Val=(int)(lon-(Deg_Val*100));
-		Sec_Val=((lon-(Deg_Val*100))-Min_Val)*100;
-		data->longitude=(Deg_Val+(Min_Val*Min_To_Degree)+(Sec_Val*Sec_To_Degree));
-		printf("longitude : %f\n",data->longitude);
-
-		Deg_Val=(int)((lat/100));
-		Min_Val=(int)(lat-(Deg_Val*100));
-		Sec_Val=((lat-(Deg_Val*100))-Min_Val)*10;
-		data->latitude=(Deg_Val+(Min_Val*Min_To_Degree)+(Sec_Val*Sec_To_Degree));
-		printf("latitude : %f\n",data->latitude);
-	}
-	    /*hr=(int)(data->time)/10000;
-	    min=(int)(data->time-(hr*10000))/100;
-	    sec=(int)(data->time-((hr*10000)+(min*100)));*/
-
-
-	    hr=(int)((data->time)/10000);
-	   	min=(int)(data->time-(hr*10000))/100;
-	   	sec=(int)(data->time-((hr*10000)+(min*100)));
-	   	int ist_hr = hr + 5;  // 5 hours difference
-	   	int ist_min = min + 30;  // 30 minutes difference
-        if(ist_hr>=24)
-        {
-        	ist_hr-=24;
-        }
-	   	// Adjusting for overflow
-	   	if (ist_min >= 60) {
-	   	    ist_hr++;
-	   	    ist_min -= 60;
-	   	}
-	    //sprintf(time_buf, "%02d:%02d:%02d",hr,min,sec);
-	    dd=(data->Date)/10000;
-	    mm=(data->Date-(dd*10000))/100;
-	    yy=(data->Date-((dd*10000)+(mm*100)));
-	    printf("time: %02d:%02d:%02d\n",ist_hr,ist_min,sec);
-	    printf("date: %02d/%02d/%02d\n",dd,mm,yy);
-		sprintf(gps_info,"latitude\":%f,\"longitude\":%f,\"time\": \%02d:%02d:%02d,\date\: \%02d/%02d/%02d",data->latitude, data->longitude,ist_hr,ist_min,sec,dd,mm,yy);
-		//printf("json format is %s\n",json_str);
-	   	//memset(data->Data,0,sizeof(data->Data));
-		//memset(data->buffer,0,sizeof(data->buffer));
-		//memset(data->rmc_buffer,0,sizeof(data->rmc_buffer));
-}
-//void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
 
 /* USER CODE END 0 */
 
@@ -283,43 +86,36 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_UART4_Init();
-  MX_UART5_Init();
-  MX_USART1_UART_Init();
-  MX_USART2_UART_Init();
-  MX_USART3_UART_Init();
-
-  //HAL_UART_Receive_IT(&huart2, gps.Data, sizeof(gps.Data));
   /* USER CODE BEGIN 2 */
- // HAL_UART_Receive_IT(&huart2, gps.Data, sizeof(gps.Data));
+  char readData[86];
+    char dataToWrite[] ="{\n  \"Date\":070224,\n  \"Time\": 120145,\n  \"Latitude\": 1727.405,\n  \"Longitude\": 7822.510\n}";
+int len = strlen(dataToWrite);
+    /************** Erase a flash page *************/
+    eraseFlashPage(FLASH_START_ADDRESS);
+    HAL_Delay(1000);
+
+int i;
+    for(i=0;i<20;i++){
+    /************* Write data to flash *************/
+    writeStringToFlash(FLASH_START_ADDRESS+88*i,dataToWrite);
+    HAL_Delay(1000);
+    readDataFromFlash(FLASH_START_ADDRESS+88*i, readData, sizeof(dataToWrite) / sizeof(char));
+    printf("%s\n",readData);
+    }
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-//gps_init();
-  //s_init();
-
   while (1)
   {
-	  printf("in loop\n");
     /* USER CODE END WHILE */
-	   // memset(gps.Data,0,750);
-    	//HAL_UART_Receive(&huart3,(uint8_t *)gps.Data,750,2000);
-    	//printf("%s\n",gps.Data);
-    	//gpsData(&gps);
-	   // gps_init();
-	  HAL_UART_Receive_IT(&huart1, (uint8_t *)gps.Data, sizeof(gps.Data));
-	  HAL_Delay(5000);
-        printf("%s\n",gps_info);
-        printf("erase started\n");
-        eraseFlashPage(FLASH_START_ADDRESS);
-        printf("erased\n");
-        writeStringToFlash(FLASH_START_ADDRESS,gps_info);
-        printf("write done\n");
-
-    /* USER CODE BEGIN 3 */
+	  /**************  Read data from flash ************/
+//	     readDataFromFlash(FLASH_START_ADDRESS, readData, sizeof(dataToWrite) / sizeof(char));
+//   printf("%s\n",readData);
+	     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
+
 }
 
 /**
@@ -341,11 +137,16 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
-  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
-  RCC_OscInitStruct.MSICalibrationValue = 0;
-  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 10;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -355,190 +156,15 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
   {
     Error_Handler();
   }
-}
-
-/**
-  * @brief UART4 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_UART4_Init(void)
-{
-
-  /* USER CODE BEGIN UART4_Init 0 */
-
-  /* USER CODE END UART4_Init 0 */
-
-  /* USER CODE BEGIN UART4_Init 1 */
-
-  /* USER CODE END UART4_Init 1 */
-  huart4.Instance = UART4;
-  huart4.Init.BaudRate = 9600;
-  huart4.Init.WordLength = UART_WORDLENGTH_8B;
-  huart4.Init.StopBits = UART_STOPBITS_1;
-  huart4.Init.Parity = UART_PARITY_NONE;
-  huart4.Init.Mode = UART_MODE_TX_RX;
-  huart4.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart4.Init.OverSampling = UART_OVERSAMPLING_16;
-  huart4.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  huart4.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart4) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN UART4_Init 2 */
-
-  /* USER CODE END UART4_Init 2 */
-
-}
-
-/**
-  * @brief UART5 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_UART5_Init(void)
-{
-
-  /* USER CODE BEGIN UART5_Init 0 */
-
-  /* USER CODE END UART5_Init 0 */
-
-  /* USER CODE BEGIN UART5_Init 1 */
-
-  /* USER CODE END UART5_Init 1 */
-  huart5.Instance = UART5;
-  huart5.Init.BaudRate = 9600;
-  huart5.Init.WordLength = UART_WORDLENGTH_8B;
-  huart5.Init.StopBits = UART_STOPBITS_1;
-  huart5.Init.Parity = UART_PARITY_NONE;
-  huart5.Init.Mode = UART_MODE_TX_RX;
-  huart5.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart5.Init.OverSampling = UART_OVERSAMPLING_16;
-  huart5.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  huart5.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart5) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN UART5_Init 2 */
-
-  /* USER CODE END UART5_Init 2 */
-
-}
-
-/**
-  * @brief USART1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART1_UART_Init(void)
-{
-
-  /* USER CODE BEGIN USART1_Init 0 */
-
-  /* USER CODE END USART1_Init 0 */
-
-  /* USER CODE BEGIN USART1_Init 1 */
-
-  /* USER CODE END USART1_Init 1 */
-  huart1.Instance = USART1;
-  huart1.Init.BaudRate = 9600;
-  huart1.Init.WordLength = UART_WORDLENGTH_8B;
-  huart1.Init.StopBits = UART_STOPBITS_1;
-  huart1.Init.Parity = UART_PARITY_NONE;
-  huart1.Init.Mode = UART_MODE_TX_RX;
-  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART1_Init 2 */
-
-  /* USER CODE END USART1_Init 2 */
-
-}
-
-/**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART2_UART_Init(void)
-{
-
-  /* USER CODE BEGIN USART2_Init 0 */
-
-  /* USER CODE END USART2_Init 0 */
-
-  /* USER CODE BEGIN USART2_Init 1 */
-
-  /* USER CODE END USART2_Init 1 */
-  huart2.Instance = USART2;
-  huart2.Init.BaudRate = 9600;
-  huart2.Init.WordLength = UART_WORDLENGTH_8B;
-  huart2.Init.StopBits = UART_STOPBITS_1;
-  huart2.Init.Parity = UART_PARITY_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART2_Init 2 */
-
-  /* USER CODE END USART2_Init 2 */
-
-}
-
-/**
-  * @brief USART3 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART3_UART_Init(void)
-{
-
-  /* USER CODE BEGIN USART3_Init 0 */
-
-  /* USER CODE END USART3_Init 0 */
-
-  /* USER CODE BEGIN USART3_Init 1 */
-
-  /* USER CODE END USART3_Init 1 */
-  huart3.Instance = USART3;
-  huart3.Init.BaudRate = 9600;
-  huart3.Init.WordLength = UART_WORDLENGTH_8B;
-  huart3.Init.StopBits = UART_STOPBITS_1;
-  huart3.Init.Parity = UART_PARITY_NONE;
-  huart3.Init.Mode = UART_MODE_TX_RX;
-  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
-  huart3.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  huart3.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART3_Init 2 */
-
-  /* USER CODE END USART3_Init 2 */
-
 }
 
 /**
@@ -552,50 +178,15 @@ static void MX_GPIO_Init(void)
 /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOD_CLK_ENABLE();
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
 }
-/*void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
-{
-    // Handle received data
-    //gpsData(&gps);
-//	flag=1;
 
-    // Restart UART receive
-    HAL_UART_Receive_IT(&huart2, gps.Data, sizeof(gps.Data));
-    gpsData(&gps);
-}*/
 /* USER CODE BEGIN 4 */
 
-
 /* USER CODE END 4 */
-
-/**
-  * @brief  Period elaps
-  * ed callback in non blocking mode
-  * @note   This function is called  when TIM2 interrupt took place, inside
-  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
-  * a global variable "uwTick" used as application time base.
-  * @param  htim : TIM handle
-  * @retval None
-  */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
-  /* USER CODE BEGIN Callback 0 */
-
-  /* USER CODE END Callback 0 */
-  if (htim->Instance == TIM2) {
-    HAL_IncTick();
-  }
-  /* USER CODE BEGIN Callback 1 */
-
-  /* USER CODE END Callback 1 */
-}
 
 /**
   * @brief  This function is executed in case of error occurrence.
