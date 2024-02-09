8e92cd3eb69093c9b616639a046b45c360c0b3de
mahimadec <142892553+mahimadec@users.noreply.github.com>
2024-02-09 16:45:50 +0530

    Update gsm 9/2/2024
     added comments

--- 08_02_24/source/gsm.c
+++ 08_02_24/source/gsm.c
@@ -19,19 +19,27 @@ uint8_t rx[300];
 uint8_t var=0;
 
 int k=0;
+
+/*check signal quality*/
+
 int signal_qualityy()
 {
-	int a=0;
-	 a=send_at_fun("AT+CSQ\r\n",check_signal);
+	int check_datcheck_data=0;
+	 check_datcheck_data=send_at_fun("AT+CSQ\r\n",check_signal);   //checking signal quality
 	 return a;
 }
+
+
+
+/* sending data to server */
+
 int send_mesg_to_server(char *mes,int size)
 {
 		char buf[20];
 		memset(buf,'\0',sizeof(buf));
-		sprintf(buf, "AT+CIPSEND=2,%d\r", strlen(mes));
+		sprintf(buf, "AT+CIPSEND=2,%d\r", strlen(mes));     // server 2 is open and size of message
 		memset(rx,'\0',sizeof(rx));
-		HAL_UART_Transmit_IT(&huart4,(uint8_t*)buf,16);
+		HAL_UART_Transmit_IT(&huart4,(uint8_t*)buf,16);     // data
 		HAL_UART_Receive(&huart4, rx, 200,3000);
 		printf("Received data %s\n",rx);
 		memset(rx,'\0',sizeof(rx));
@@ -51,6 +59,11 @@ int send_mesg_to_server(char *mes,int size)
 	return 1;
 }
 
+
+/*checking  AT command  and Response */
+
+
+
 int check(char *command, char *response)
 {
 	 if(strstr(command,response)!=NULL)
@@ -68,6 +81,8 @@ int check(char *command, char *response)
 	 printf("response data is %s\n",response);
   }
 
+
+/* checking signal range values is valid or not */
 int check_signal(char *response)
 {
 	  int rssi = 0, ber = 0;
@@ -95,6 +110,10 @@ int check_signal(char *response)
 }
 
 
+
+/* sending AT command and response is passing to another function */
+
+
 int send_at(char *command,char *response)
 {
 	memset(rx,'\0',300);
@@ -105,6 +124,11 @@ int send_at(char *command,char *response)
 	 var=check(rx,response);
 	 return var;
 }
+
+
+/*checking registration  AT command response*/
+
+
 int creg(char *response)
 {
 	int stat = 0;
@@ -127,6 +151,11 @@ int creg(char *response)
 	     	 return 1;
 	    }
 }
+
+
+/* GPRS internet connection */
+
+
 int gprs(char *response)
 {
 
@@ -143,6 +172,12 @@ int gprs(char *response)
 
     printf("GPRS attached successfully\n");
 }
+
+
+
+/* send AT command and response is called another function */
+
+
 int send_at_fun(char *command,void *(fp)(char *))
 {
 
@@ -157,6 +192,7 @@ int send_at_fun(char *command,void *(fp)(char *))
 }
 
 
+/* Transmit AT commands and receiving response */
 
 void send(char *command,char *data)
 {
@@ -174,9 +210,11 @@ void send(char *command,char *data)
 	 HAL_Delay(3000);
 
 }
+
+
 void ful_flow()
 {
-			send_at("ATE0\r\n", "OK");
+			send_at("ATE0\r\n", "OK");    
 		  	send_at("AT+CSQ\r\n", "CSQ");
 		  	send_at("AT+CFUN?\r\n","+CFUN: 1");
 		  	send_at("AT+CREG?\r\n","+CREG: 0,6");
@@ -196,16 +234,19 @@ void ful_flow()
 		  	send_at("AT+CIPCLOSE=0\r\n", "OK");
 }
 
+
+/*checking APN operators name */
+
 int gsm_apn(void)
 {
 
-	int a=0,b=0;
-			a=send_at("AT+COPS?\r\n","405854");
-			if(a==1)
+	int check_datcheck_data=0,check_con0;
+			check_datcheck_data=send_at("AT+COPS?\r\n","405854");
+			if(check_datcheck_data==1)
 			{
 				printf("match found\n");
-									b=send_at("AT+CGDCONT=1,\"IP\",\"jionet\"\r\n","OK");
-									if(b==1)
+									check_consend_at("AT+CGDCONT=1,\"IP\",\"jionet\"\r\n","OK");
+									if(check_con=1)
 									{
 										printf("set for jionet\n");
 										return 1;
@@ -219,13 +260,13 @@ int gsm_apn(void)
 			}
 			else
 			{
-				a=send_at("AT+COPS?\r\n","40449"); //
-				if(a==1)
+				check_datcheck_data=send_at("AT+COPS?\r\n","40449"); //
+				if(check_datcheck_data==1)
 				{
 
 					              printf("match found\n");
-									b=send_at("AT+CGDCONT=1,\"IP\",\"airtelgprs.com\"\r\n","OK");
-									if(b==1)
+									check_consend_at("AT+CGDCONT=1,\"IP\",\"airtelgprs.com\"\r\n","OK");
+									if(check_con=1)
 									{
 										printf("set for airtel\n");
 										return 1;
@@ -242,29 +283,31 @@ int gsm_apn(void)
 	}
 
 
+/* GSM BASIC AT COMMANDS INITIALIZATION */
+
 
 void gsm_condition()
 {
 	    int state=1;
 	    int substate = 1;
-	    int a,b=0;
+	    int check_data,check_con0;
 	    int count=0;
 	 while(state==1)
 	 {
 	    switch (substate)
 	    {
-	    	a=0;
+	    	check_datcheck_data=0;
 	        case 1:
 	            printf("at\n");
-	            a=send_at("ATE0\r\n", "OK");
-	            if(a==1)
+	            check_datcheck_data=send_at("ATE0\r\n", "OK");    //ATE0 commands and response ok
+	            if(check_datcheck_data==1)
 	            {
 	            printf("Case 1 is Matched.\n");
 	            substate=2;
 	            }
-	            else if(b==10)
+	            else if(check_con=10)
 	            {
-	            	b=0;
+	            	check_con0;
 	             state=0;
 	            }
 	            else{
@@ -275,8 +318,8 @@ void gsm_condition()
 
 	        case 2:
 	           printf("cpin?\n");
-	           a=send_at("AT+CPIN?\r\n", "+CPIN: READY");
-	            if(a==1)
+	           check_datcheck_data=send_at("AT+CPIN?\r\n", "+CPIN: READY");  // +CPIN SIM READY
+	            if(check_datcheck_data==1)
 	            {
 	            printf("Case 2 is Matched.\n");
 	            substate=8;
@@ -288,8 +331,8 @@ void gsm_condition()
 
 	        case 3:
 	            printf("csq\n");
-	            a=send_at_fun("AT+CSQ\r\n",check_signal);
-	            if(a==1)
+	            check_data=send_at_fun("AT+CSQ\r\n",check_signal);  // CHECK SIGNAL QUALITY
+	            if(check_data==1)
 	            {
 	            printf("Case 3 is Matched.\n");
 	            substate=4;
@@ -301,15 +344,15 @@ void gsm_condition()
 	            }
 	            else{
 	                printf("at+cfun=1\n");
-	              //  send_at("AT+CFUN=1\r\n","OK");
+	              //  send_at("AT+CFUN=1\r\n","OK");        // CHECK FUNCTIONALITY 
 	                substate=3;
 	                count++;
 	            }
 	            break;
 	        case 4:
 	            printf("creg?\n");
-	            a=send_at_fun("AT+CREG?\r\n",creg);
-	            if(a==1)
+	            check_data=send_at_fun("AT+CREG?\r\n",creg);   //CHECK SIM REGISTRATION
+	            if(check_data==1)
 	            {
 	            printf("Case 4 is Matched.\n");
 	            substate=5;
@@ -320,8 +363,8 @@ void gsm_condition()
 	            break;
 	        case 5:
 	            printf("cgreg?\n");
-	            a=send_at("AT+CGREG?\r\n","+CGREG");
-	            if(a==1)
+	            check_data=send_at("AT+CGREG?\r\n","+CGREG");  // CHECK GSM Network Registration Status
+	            if(check_data==1)
 	            {
 	            printf("Case 5 is Matched.\n");
 	            substate=6;
@@ -332,8 +375,8 @@ void gsm_condition()
 	            break;
 	        case 6:
 	            printf("cpsi?\n");
-	            a=send_at("AT+CPSI?\r\n","+CPSI: LTE,Online");
-	            if(a==1)
+	            check_data=send_at("AT+CPSI?\r\n","+CPSI: LTE,Online"); //Cellular Positioning System Interface
+	            if(check_data==1)
 	            {
 	            printf("Case 6 is Matched.\n");
 	            substate=7;
@@ -343,10 +386,10 @@ void gsm_condition()
 	            }
 	            break;
 	        case 7:
-	            printf("cgdcont\n");
-	           // a=send_at("AT+CGDCONT=1,\"IP\",\"jionet\"\r\n","OK");
-	            a= gsm_apn();
-	            if(a==1)
+	            printf("cgdcont\n");//Check query the parameters of the Packet Data Protocol (PDP) context for establishing a data connection.
+	           // check_data=send_at("AT+CGDCONT=1,\"IP\",\"jionet\"\r\n","OK"); 
+	            check_data= gsm_apn();
+	            if(check_data==1)
 	            {
 	            printf("Case 7 and apn checking is Matched.\n");
 	            state=0;
@@ -358,9 +401,9 @@ void gsm_condition()
 	            break;
 	        case 8:
 	            printf("AT+CMEE=2\n");
-	            a=send_at("AT+CMEE=2\r\n","OK");
-	            a=1;
-	            if(a==1)
+	            check_data=send_at("AT+CMEE=2\r\n","OK"); // checking the error string
+	            check_data=1;
+	            if(check_data==1)
 	            {
 	            printf("Case 8 is Matched.\n");
 	            substate=3;
@@ -372,8 +415,8 @@ void gsm_condition()
 	            break;
 	      /*  case 9:
 	            printf("AT+CGACT=1,1\n");
-	            a=send_at("AT+CGACT=1,1\r\n","OK");
-	            if(a==1)
+	            check_data=send_at("AT+CGACT=1,1\r\n","OK");   //check control the activation or deactivation of a PDP
+	            if(check_data==1)
 	            {
 	            printf("Case 9 is Matched.\n");
 	            //substate=10;
@@ -387,28 +430,35 @@ void gsm_condition()
 	       }
 	  }
 }
+
+
+
+/*CHECKING and opening the TCP server*/ 
+
+
+
 void tcp_ip_protocol()
 	{
 		int state1=1;
 		int substate1=0;
-		int a=0,b=0,count=0,c=0;
+		int check_data=0,check_con0,count=0,c=0;
 		while(state1==1)
 		{
-			a=0;b=0;
+			check_data=0;check_con0;
 			switch(substate1)
 			{
 
 
 				case 0:
-					a=send_at("AT+CIPMODE?\r\n", "+CIPMODE: 0");
-					if(a==1)
+					check_data=send_at("AT+CIPMODE?\r\n", "+CIPMODE: 0");   //check TCP/IP protocol stack for internet communication.
+					if(check_data==1)
 					{
 						substate1=1;
 					}
 					else
 					{
-						b=send_at("AT+CIPMODE=0\r\n", "OK");
-						if(b==1)
+						check_consend_at("AT+CIPMODE=0\r\n", "OK");    //TCP/IP protocol stack for internet communication
+						if(check_con=1)
 						{
 							substate1=1;
 						}
@@ -430,8 +480,8 @@ void tcp_ip_protocol()
 					}break;
 				case 1:
 
-					a=send_at("AT+NETOPEN\r\n", "NETOPEN");
-					if(a==1)
+					check_data=send_at("AT+NETOPEN\r\n", "NETOPEN");  // Telit cellular modules, such as those used in IoT (Internet of Things) applications
+					if(check_data==1)
 					{
 						substate1=3;
 					 }
@@ -442,8 +492,8 @@ void tcp_ip_protocol()
 
 					 break;
 				case 2:
-							a=send_at("AT+NETOPEN\r\n", "opened");
-							if(a==1)
+							check_data=send_at("AT+NETOPEN\r\n", "opened"); 
+							if(check_data==1)
 							{
 								substate1=3;
 							}
@@ -454,12 +504,12 @@ void tcp_ip_protocol()
 							break;
 
 				 case 3:
-					 a=send_at("AT+CIPOPEN?\r\n","139.59.78.252");
+					 check_data=send_at("AT+CIPOPEN?\r\n","139.59.78.252");  //  establishing a TCP or UDP connection over a GPRS (General Packet Radio Service) network
 					 printf("ip already network opened\n");
-						if(a==1)
+						if(check_data==1)
 						{
 							 //send("AT+CIPSEND=0,5\r","TEAMC\r");
-							// a=send_at("AT+CIPSEND=0,5\r","");
+							// check_data=send_at("AT+CIPSEND=0,5\r","");
 							send_mesg_to_server("TEAMC",5);
 							 //substate1=3;
 							state1=0;
@@ -470,8 +520,8 @@ void tcp_ip_protocol()
 						}
 						break;
 				 case 4:
-					 a=send_at("AT+CIPOPEN=2,\"TCP\",\"139.59.78.252\",52102\r","CIPOPEN: 2,0");
-					 if(a==1){
+					 check_data=send_at("AT+CIPOPEN=2,\"TCP\",\"139.59.78.252\",52102\r","CIPOPEN: 2,0"); // check the server opened or not
+					 if(check_data==1){
 						 printf("ip network opened\n");
 						 //send("AT+CIPSEND=0,5\r","TEAMC\r");
 
@@ -487,8 +537,8 @@ void tcp_ip_protocol()
 					 break;
 
 				 case 5:
-				 	 	 a=send_at("AT+CIPCLOSE=2\r","+CIPCLOSE: 2");
-				 	 	 if(a==1)
+				 	 	 check_data=send_at("AT+CIPCLOSE=2\r","+CIPCLOSE: 2");  //server close 
+				 	 	 if(check_data==1)
 				 	 	 {
 				 	 		 printf("closed");
 				 	 		 state1=0;
@@ -516,33 +566,43 @@ void tcp_ip_protocol()
 	}
 
 
+
+/* check GPRS Connection or not */
+
+
 int gprs_check(void)
 {
-	int  a=0;
+	int  check_data=0;
 			  printf("AT+CGATT=1\n");
-		      a= send_at("AT+CGATT=1\r\n","OK");
+		      check_data= send_at("AT+CGATT=1\r\n","OK");
 		      printf("gprs working\n");
-		      return a;
+		      return check_data;
 }
+
+
+
+/*CHECKING and opening the TCP server and sending data*/
+
+
 int tcp_server_open(char *data)
 {
 
 	int state1=1;
 	int substate1=1;
-	int a=0,b=0,count=0;
+	int check_data=0,check_con0,count=0;
 	int size=strlen(data);
 	while(state1==1)
 	{
-		a=0;b=0;
+		check_data=0;check_con0;
 		switch(substate1)
 		{
 				 case 1:
-									 a=send_at("AT+CIPOPEN?\r\n","139.59.78.252");
+									 check_data=send_at("AT+CIPOPEN?\r\n","139.59.78.252");
 									 printf("ip already network opened\n");
-										if(a==1)
+										if(check_data==1)
 										{
 											 //send("AT+CIPSEND=0,5\r","TEAMC\r");
-											// a=send_at("AT+CIPSEND=0,5\r","");
+											// check_data=send_at("AT+CIPSEND=0,5\r","");
 											send_mesg_to_server(data,size);
 											// substate1=1;
 											return 1;
@@ -554,8 +614,8 @@ int tcp_server_open(char *data)
 										}
 										break;
 								 case 2:
-									 a=send_at("AT+CIPOPEN=2,\"TCP\",\"139.59.78.252\",52102\r","CIPOPEN: 2,0");
-									 if(a==1){
+									 check_data=send_at("AT+CIPOPEN=2,\"TCP\",\"139.59.78.252\",52102\r","CIPOPEN: 2,0");
+									 if(check_data==1){
 										 printf("ip network opened\n");
 										 //send("AT+CIPSEND=0,5\r","TEAMC\r");
 
@@ -569,19 +629,19 @@ int tcp_server_open(char *data)
 									 }
 									 break;
 								 case 3:
-												 	 		 	 	 	 	if(count==2)
-												 	 						{
-												 	 		 	 	 	 		count=0;
-												 	 							state1=0;
+									if(count==2)
+									{
+										count=0;
+										state1=0;
 
-												 	 						}
-												 	 		 	 	 	 	else
-												 	 		 	 	 	 	{
-												 	 		 	 	 	 	substate1=1;
-												 	 		 	 	 	 	}
-												 	 						count++;
+									}
+									else
+									{
+										substate1=1;
+									}
+									count++;
 
-												 	 							break;
+									break;
 		}
 	}
 }
